35068e0849815889bb313c5141fc337356a0f29b
safabalo <safaabalha58@gmail.com>
2022-03-08 10:21:46 +0000

    calcule 1

--- js/fonctions.js
+++ js/fonctions.js
@@ -1,10 +1,56 @@
-function Addnumber(){
+let number = '';
+let Firstnumber = '';
+let result = '';
+let history = '';
 
+function calcule(index){ 
+    if(index =='.'){
+        if(number!= '' && !number.includes('.')){
+            number =  number + index ;
+        document.getElementById("display").innerHTML = number; 
+        }
+    
+    }else{
+        number =  number + index ;
+        document.getElementById("display").innerHTML = number; 
+    }   
 }
 
-function Multiplynumber(){
-
+function operation(operate){
+    Firstnumber = number;
+    number = '';
+    if(operate == '+'){
+        history = Firstnumber + operate;
+        document.getElementById("operation").innerHTML = history;
+    }
+    if(operate == '-'){
+        history = Firstnumber + operate;
+        document.getElementById("operation").innerHTML = history;
+    }
+    if(operate == 'x'){
+        history = Firstnumber + operate;
+        document.getElementById("operation").innerHTML = history;
+    }
+    
 }
-function DivideNumber(){
 
+function equals(){
+
+    if(history.includes('+')){
+        result = parseInt(Firstnumber) + parseInt(number);
+        document.getElementById("display").innerHTML = result;
+        document.getElementById("operation").innerHTML = (`${Firstnumber} + ${number} =`);
+    }
+    if(history.includes('-')){
+        result = parseInt(Firstnumber) - parseInt(number);
+        document.getElementById("display").innerHTML = result;
+        document.getElementById("operation").innerHTML = (`${Firstnumber} - ${number} =`);
+    }
+    if(history.includes('x')){
+        result = parseInt(Firstnumber) * parseInt(number);
+        document.getElementById("display").innerHTML = result;
+        document.getElementById("operation").innerHTML = (`${Firstnumber} x ${number} =`);
+    }
 }
+    
+
