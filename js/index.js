062099f5672037edb55d93bb5067418b3584210b
safabalo <safaabalha58@gmail.com>
2022-03-14 09:42:06 +0000

    calculator finale

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let form = document.querySelector('form')
+let taskInput = document.querySelector('#task_input')
+let taskContainer = document.getElementById('task_container')
+let deleteIcon = document.getElementById('delete')
+form.addEventListener('submit',function(e),)
+
+let task = `
+<div class= "changement">
+<p></p>
+<i></i>
+</div>
+`
